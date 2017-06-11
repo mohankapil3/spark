c916525295a681c4cfaeed449056baa305427f75
kmo <mohankapil@gmail.com>
2017-06-11 22:51:18 +0100

    data parser interim commit

--- monte-carlo/src/main/resources/download-symbol.sh
+++ monte-carlo/src/main/resources/download-symbol.sh
@@ -4,4 +4,6 @@
 #
 # See LICENSE file for further information.
 
+echo Downloading data for $1
+
 curl -o $2/$1.csv "http://www.google.com/finance/historical?q=NASDAQ:$1&startdate=Oct+23%2C+2009&enddate=Oct+23%2C+2014&output=csv"
--- monte-carlo/src/main/scala/com/mcl/mcsim/data/DataParser.scala
+++ monte-carlo/src/main/scala/com/mcl/mcsim/data/DataParser.scala
@@ -1,5 +1,10 @@
+package com.mcl.mcsim.data
+
+import java.io.File
+import com.github.nscala_time.time.Imports._
+
 object DataParser {
 
-  
+  def readInvestingDotComHistory(file: File): Array[(DateTime, Double)] = ???
 
 }
