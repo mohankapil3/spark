5ead462bd1ab40e16cae67419964b864c9d2ce88
kmo <mohankapil@gmail.com>
2017-09-08 08:03:00 +0100

    minor refactoring

--- monte-carlo/src/test/scala/com/mcl/mcsim/data/DataParserSuite.scala
+++ monte-carlo/src/test/scala/com/mcl/mcsim/data/DataParserSuite.scala
@@ -8,7 +8,7 @@ class DataParserSuite extends FunSuite with Matchers {
 
   test("Investing dot com history CSV file should be parseable") {
 
-    val testHistoryFile = new File(getClass.getResource("/test_Crude_Oil_Historical_Prices-Investing.com-UK.csv").toURI)
+    val testHistoryFile = new File(getClass.getResource("/test_crude_oil.csv").toURI)
     val tuples = DataParser.readInvestingDotComHistoryCSV(testHistoryFile)
     tuples.length should be (21)
     tuples.last._2 should be (45.77)
