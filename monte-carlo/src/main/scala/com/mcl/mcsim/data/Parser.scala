f392c222d7e7a961c99a78c3344780f8eb218c47
kmo <mohankapil@gmail.com>
2017-09-14 22:20:08 +0100

    completed source data parsing and cleansing

--- monte-carlo/src/main/scala/com/mcl/mcsim/data/Parser.scala
+++ monte-carlo/src/main/scala/com/mcl/mcsim/data/Parser.scala
@@ -1,19 +1,16 @@
 package com.mcl.mcsim.data
 
 import java.io.File
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-
-import com.github.nscala_time.time.Imports._
 
 import scala.io.Source
 
 object Parser {
 
-  def readInvestingDotComHistoryCSV(file: File): Array[(DateTime, Double)] = {
+  def readInvestingDotComHistoryCSV(file: File): Array[(LocalDate, Double)] = {
 
-    val format = new SimpleDateFormat("MMM d, yyyy")
+    val format = DateTimeFormatter.ofPattern("MMM d, yyyy")
     val rawLines = Source.fromFile(file).getLines().toArray
     // first line is header and last 2 lines are to be ignored
     val lines = rawLines.slice(1, rawLines.length - 2)
@@ -21,7 +18,7 @@ object Parser {
     lines.map(line => {
       val cols = line.split(",(?=([^\"]*\"[^\"]*\")*(?![^\"]*\"))")
       val dateAsText = removeLeadingTrailingQuotes(cols(0))
-      val date = new DateTime(format.parse(dateAsText))
+      val date = LocalDate.parse(dateAsText, format)
       val value = removeLeadingTrailingQuotes(cols(1)).toDouble
       (date, value)
     }).reverse
