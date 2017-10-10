7818d643f7cb49fcfee9f3b6dc82633d133e259b
kmo <mohankapil@gmail.com>
2017-10-10 23:51:24 +0100

    added functions to perform linear regression

--- monte-carlo/build.sbt
+++ monte-carlo/build.sbt
@@ -4,5 +4,6 @@ version := "0.1"
 
 scalaVersion := "2.10.6"
 
+libraryDependencies += "org.apache.commons" % "commons-math3" % "3.6.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.3" % "test"
 
--- monte-carlo/src/main/scala/com/mcl/mcsim/data/Projector.scala
+++ monte-carlo/src/main/scala/com/mcl/mcsim/data/Projector.scala
@@ -1,5 +1,7 @@
 package com.mcl.mcsim.data
 
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression
+
 import scala.reflect.ClassTag
 
 object Projector {
@@ -11,6 +13,25 @@ object Projector {
       result(i) = collection.map(_(i)).toArray
     }
     result
+  }
+
+  def featurize(factorReturns: Array[Double]): Array[Double] = {
+    val squareReturns = factorReturns.map(x => math.signum(x) * x * x)
+    val squareRootedReturns = factorReturns.map(x => math.signum(x) * math.sqrt(math.abs(x)))
+    squareReturns ++ squareRootedReturns ++ factorReturns
+  }
+
+  def linearModel(instrument: Array[Double], factorMatrix: Array[Array[Double]]): OLSMultipleLinearRegression = {
+    val regression = new OLSMultipleLinearRegression()
+    regression.newSampleData(instrument, factorMatrix)
+    regression
+  }
+
+  def getFactorWeights(stockReturns: Seq[Array[Double]], factorReturns: Seq[Array[Double]]) = {
+    val factorMatrix = transpose(factorReturns)
+    val factorFeatures = factorMatrix.map(featurize)
+    val models = stockReturns.map(linearModel(_, factorFeatures))
+    models.map(_.estimateRegressionParameters()).toArray
   }
 
 }
